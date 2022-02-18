6eb1cd4a2e545dbf3446b9fbe7909f9190e2157c
vaibd <git@cheems.anonaddy.com>
2022-02-18 18:48:40 +0530

    Implemented Components and props for easy reuse of the code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Footer from "./Footer";
 import Header from "./Header";
+import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((notes) => (
+        <Note key={notes.key} title={notes.title} content={notes.content} />
+      ))}
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-let year = new Date().getFullYear();
-
 function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright © {year}</p>
+      <p>Copyright ⓒ {year}</p>
     </footer>
   );
 }
